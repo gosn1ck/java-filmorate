a5fdd50b82ff435262d2fe6faff7926521a4f4db
Николай Верховцев <gosn1ck@yandex.ru>
2023-03-19 23:04:36 +0300

    fix: move classes to packages

--- src/main/java/ru/yandex/practicum/filmorate/controller/ApiExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ApiExceptionHandler.java
@@ -1,10 +1,13 @@
-package ru.yandex.practicum.filmorate.exception;
+package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.yandex.practicum.filmorate.model.ApiException;
+import ru.yandex.practicum.filmorate.exception.InternalServerException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
 import java.time.ZonedDateTime;
 
