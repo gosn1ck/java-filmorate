05578e58e3a7f2fab50f7cb0f60846862b28667c
Николай Верховцев <gosn1ck@yandex.ru>
2023-04-09 00:28:17 +0300

    fix: empty line separator

--- src/main/java/ru/yandex/practicum/filmorate/repository/LikeRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/LikeRepository.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public interface LikeRepository {
     void save(Film film, User user);
+
     void delete(Film film, User user);
+
     List<Film> popularFilms(List<Film> films, Integer count);
+
 }
